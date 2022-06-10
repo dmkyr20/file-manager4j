1a61bed225281fca54ead7c5a3180e855ea49426
Dmytro_Kyrychkov <dima@syncwords.com>
2022-06-10 21:50:33 +0200

    fix

--- core/src/main/java/com/dmkyr20/filemanager/core/base/MetadataViewer.java
+++ core/src/main/java/com/dmkyr20/filemanager/core/base/MetadataViewer.java
@@ -1,14 +1,8 @@
 package com.dmkyr20.filemanager.core.base;
 
-import com.dmkyr20.filemanager.commander.Command;
-
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public interface MetadataViewer {
     BasicFileAttributes getBasicData(Path file);
-
-    enum MetadataViewCommand implements Command {
-        SHOW_BASIC_DATA
-    }
 }
